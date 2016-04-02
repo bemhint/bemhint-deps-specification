641a5a30c7a63a4286285a3828ef1a153bbe7a44
Sergey Berezhnoy <veged@yandex-team.ru>
2016-04-02 20:26:07 +0300

    refactoring!11

--- index.js
+++ index.js
@@ -1,349 +1,127 @@
-/**
- * Helper to recognize type of variable
- * @param obj
- * @returns {string}
- */
-var toType = function(obj) {
-  return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
-};
+var _ = require('lodash'),
+    vm = require('vm');
 
-/**
- * String validation helper, that throw exceptions if variable is not a string
- * @param val
- * @param [fieldName]
- * @returns {Boolean|String}
- */
-var simpleStringValidation = function(val, fieldName) {
-    var valueType = toType(val);
+function stringValidation(val, field, addError, expected) {
+    _.isString(val) || addError(
+        'Invalid type (' + typeof val + ')' +
+        ' in "' + field + '":' +
+        ' expected ' + concatExpected(expected, 'string', ' or '));
+}
 
-    if (valueType !== 'string') {
-        return fieldName ?
-            'Invalid type at field "'+fieldName+'": '+ valueType +' (expected string)' :
-            'Invalid type: '+ valueType +' (expected string)';
-    }
-};
-
-/**
- * Array validation helper, that throw exceptions if item is not a string
- * @param {Array} arr
- * @param {String} [fieldName]
- * @returns {String}
- */
-var simpleArrayWithStringValidation = function(arr, fieldName) {
-    var haveErr = arr.some(function(item) {
-        return toType(item) !== 'string';
+function arrayOfStringsValidation(val, field, addError) {
+    val.forEach(function(innerVal, i) {
+        stringValidation(innerVal, field + '[' + i + ']', addError);
     });
+}
 
-    if (haveErr) {
-        return fieldName ?
-            'Invalid array item type in "'+fieldName+'": expected string' :
-            'Invalid array item type: expected string';
-    }
-};
+function booleanOrStringValidation(val, field, addError, expected) {
+    _.isBoolean(val) ||
+        stringValidation(
+            val,
+            field,
+            addError,
+            concatExpected(expected, 'boolean'));
+}
 
-/**
- * Validation rules dictionary for fields
- * @type {Object}
- */
-var validationRules = {
+function stringOrArrayOfStringsValidation(val, field, addError) {
+    (_.isArray(val)? arrayOfStringsValidation : stringValidation)(val, field, addError, 'array');
+}
 
-    block: {
-        valueValidation: function(block) {
-            // If we got block in object type notation, like this:
-            // { tech : 'bemhtml', block : { block : 'link', mods : { pseudo : true } } }
-            if (toType(block) === 'object') {
-                var errors = [];
+function depsItemValidation(val, field, validFields, addError, expected) {
+    _.isPlainObject(val)?
+        _.forEach(val, function(innerVal, innerField) {
+            validFields.hasOwnProperty(innerField)?
+                fieldsValidations[innerField] && fieldsValidations[innerField](innerVal, field + '.' + innerField, addError) :
+                addError('Invalid field (' + innerField + ') in "' + field + '"');
+        }) :
+        stringValidation(val, field, addError, concatExpected(expected, 'object'));
+}
 
-                Object.keys(block).forEach(function(field) {
-                    if (validationRules[field]) {
-                        var fieldErr = validationRules[field].valueValidation((block[field]));
+function arrayOfDepsItemsValidation(val, field, validFields, addError) {
+    val.forEach(function(innerVal, i) {
+        depsItemValidation(innerVal, field + '[' + i + ']', validFields, addError);
+    });
+}
 
-                        (fieldErr || []).length && (errors.push(fieldErr));
-                    } else if (!~validDeclFields.indexOf(field)) {
-                        errors.push('Invalid field ('+ field +') in declaration');
-                    }
-                }, this);
+function depsItemOrArrayOfDepsItemsValidation(val, field, validFields, addError) {
+    (_.isArray(val)? arrayOfDepsItemsValidation : depsItemValidation)(val, field, validFields, addError, 'array');
+}
 
-                if (errors.length) {
-                    return errors;
-                }
-            } else if(toType(block) !== 'string') {
-                var blockValType = toType(block);
+function topLevelDepsValidation(val, field, addError) {
+    depsItemOrArrayOfDepsItemsValidation(val, field, validTopLevelFields, addError);
+}
 
-                return 'Invalid block declaration type ('+ blockValType +'), expected string or object';
-            }
-        }
+var validElemFields = {
+        elem : true,
+        mod : true,
+        mods : true,
+        val : true,
+        tech : true,
+        mustDeps : true,
+        shouldDeps : true,
+        noDeps : true,
+        include : true
     },
+    validTopLevelFields = _.merge(validElemFields, {
+        block : true,
+        elems : true,
+    }),
+    fieldsValidations = {
+        block: stringValidation,
 
-    elem: {
-        valueValidation: function(elem) {
+        elem: stringOrArrayOfStringsValidation,
 
-            if(toType(elem) === 'array') {
-                return simpleArrayWithStringValidation(elem, 'elem');
+        elems: function(val, field, addError) {
+            depsItemOrArrayOfDepsItemsValidation(val, field, validElemFields, addError);
+        },
+
+        mod : stringValidation,
+
+        mods: function(val, field, addError) {
+            if (_.isArray(val)) {
+                arrayOfStringsValidation(val, field, addError);
+            } else if (_.isPlainObject(val)) { // If mods in object type notation, like this - { disabled : true, focused : 'yes' }
+                _.forEach(val, function(innerVal, innerField) {
+                    (_.isArray(innerVal)? arrayOfStringsValidation : booleanOrStringValidation)
+                        (innerVal, field + '.' + innerField, addError, 'array');
+                });
             } else {
-                return simpleStringValidation(elem, 'elem');
+                addError('Invalid declaration type (' + typeof val + ') in "' + field + '": expected array or object');
             }
+        },
+
+        val : booleanOrStringValidation,
+
+        tech : stringValidation,
+
+        mustDeps : topLevelDepsValidation,
+
+        shouldDeps : topLevelDepsValidation,
+
+        noDeps : topLevelDepsValidation,
+
+        include : function(val, field, addError) {
+            val === false || addError('Invalid value (' + val + ') in "' + field + '": expected false');
         }
-    },
+    };
 
-    elems: {
-        valueValidation: function(elems) {
-            var errors = [];
-
-            if (toType(elems) === 'string') {
-                var strValidation = simpleStringValidation(elems, 'elems');
-
-                if (strValidation) {
-                    errors.push(strValidation);
-                }
-            } else if (toType(elems) === 'object') {
-                Object.keys(elems).forEach(function(field) {
-                    if (validationRules[field]) {
-                        var fieldErr = validationRules[field].valueValidation((elems[field]));
-
-                        (fieldErr || []).length && (errors.push(fieldErr));
-                    } else if (!~validDeclFields.indexOf(field)) {
-                        errors.push('Invalid field ('+ field +') in declaration');
-                    }
-                }, this);
-            } else if (toType(elems) === 'array') {
-                var arrErr = simpleArrayWithStringValidation(elems, 'elems');
-
-                arrErr && errors.push(arrErr);
-            } else {
-                var elemsValType = toType(elems);
-
-                errors.push('Invalid elems declaration type ('+ elemsValType +'), expected string, array or object');
-            }
-
-            if (errors.length) {
-                return errors;
-            }
-        }
-    },
-
-    'mod': {
-        valueValidation: function(mod) {
-            return simpleStringValidation(mod, 'mod');
-        }
-    },
-
-    'mods': {
-        valueValidation: function(mods) {
-            var errors = [];
-
-            // If mods in object type notation, like this - { disabled : true, focused : 'yes' }
-            if (toType(mods) === 'object') {
-                Object.keys(mods).forEach(function(field) {
-                    var innerValue = mods[field];
-
-                    if (toType(innerValue) === 'array') {
-                        var arrErr = simpleArrayWithStringValidation(innerValue, 'mods > ' + field);
-
-                        arrErr && errors.push(arrErr);
-                    } else if (toType(innerValue) !== 'boolean') { // boolean value are valid, so skip it
-                        var fieldErr = simpleStringValidation(innerValue, 'mods > ' + field);
-
-                        if (fieldErr) {
-                            errors.push(fieldErr);
-                        }
-                    }
-                }, this);
-            } else if (toType(mods) === 'array') {
-                var arrErr = simpleArrayWithStringValidation(mods, 'mods');
-
-                arrErr && errors.push(arrErr);
-            } else {
-                var modsValType = toType(mods);
-
-                errors.push('Invalid mods declaration type ('+ modsValType +'), expected array or object');
-            }
-
-            if (errors.length) {
-                return errors;
-            }
-        }
-    },
-
-    'val': {
-        valueValidation: function(val) {
-            if (toType(val) !== 'boolean') { // boolean value are valid, so skip it
-                return simpleStringValidation(val, 'val');
-            }
-        }
-    },
-
-    'tech': {
-        valueValidation: function(tech) {
-            return simpleStringValidation(tech, 'tech');
-        }
-    },
-
-    'mustDeps' : {
-        valueValidation: function(mustDeps) {
-            var errors = [];
-
-            if (toType(mustDeps) === 'array') {
-                mustDeps.forEach(function(decl) {
-                    if (toType(decl) !== 'string') { // short string declaration are valid
-                        var innerDeclsErrors = validator.validateDeclFields(decl, validDeclFields);
-
-                        (innerDeclsErrors || []).length && errors.push(innerDeclsErrors);
-                    }
-                }, this);
-            } else if (toType(mustDeps) === 'object' && mustDeps.length === undefined) {
-                var declErrors = validator.validateDeclFields(mustDeps, validDeclFields);
-
-                (declErrors || []).length && errors.push(declErrors);
-            } else if (toType(mustDeps) !== 'string') { // short string declaration are valid
-                var declType = toType(mustDeps);
-
-                errors.push('Invalid declaration type ('+ declType +'), expected string, array or object');
-            }
-
-            if (errors.length) {
-                return errors;
-            }
-        }
-    },
-
-    'shouldDeps' : {
-        valueValidation:function(shouldDeps) {
-            var errors = [];
-
-            if (toType(shouldDeps) === 'array') {
-                shouldDeps.forEach(function(decl) {
-                    if (toType(decl) !== 'string') { // short string declaration are valid
-                        var innerDeclsErrors = validator.validateDeclFields(decl, validDeclFields);
-
-                        (innerDeclsErrors || []).length && errors.push(innerDeclsErrors);
-                    }
-                }, this);
-            } else if (toType(shouldDeps) === 'object') {
-                var declErrors = validator.validateDeclFields(shouldDeps, validDeclFields);
-
-                (declErrors || []).length && errors.push(declErrors);
-            } else if (toType(shouldDeps) !== 'string') { // short string declaration are valid
-                var declType = toType(shouldDeps);
-
-                errors.push('Invalid declaration type ('+ declType +'), expected string, array or object');
-            }
-
-            if (errors.length) {
-                return errors;
-            }
-        }
-    }
-
-};
-
-var validDeclFields = [
-    'block',
-    'elem',
-    'elems',
-    'mod',
-    'mods',
-    'val',
-    'tech',
-    'mustDeps',
-    'shouldDeps',
-    'noDeps',
-    'include'
-];
-
-var _ = require('lodash');
-
-var validator = {
-
-    validate: function(deps) {
-        var validationRes = [];
-
-        [].concat(deps).forEach(function(decl) {
-            var declValidationRes = this.validateDepsDeclaration(decl);
-
-            if (declValidationRes) {
-                validationRes = validationRes.concat(declValidationRes);
-            }
-        }, this);
-
-        if (validationRes.length) {
-
-            // Make Array of Arrays flatter for bemhint :)
-            return _.flattenDeep(validationRes);
-        } else {
-            return false;
-        }
-    },
-
-    validateDepsDeclaration: function(decl) {
-        var errors = [];
-
-        Object.keys(decl).forEach(function(field) {
-            if (validationRules[field]) {
-                var fieldErr = validationRules[field].valueValidation((decl[field]));
-
-                (fieldErr || []).length && (errors.push(fieldErr));
-            } else if (!~validDeclFields.indexOf(field)) {
-                errors.push('Invalid field ('+ field +') in declaration');
-            }
-        });
-
-        if (errors.length) {
-            return errors;
-        }
-    },
-
-    validateDeclFields: function(decl, validFields) {
-        var errors = [];
-
-        if(toType(decl) === 'object') {
-            Object.keys(decl).forEach(function(field) {
-                if (!~validFields.indexOf(field)) {
-                    errors.push('Invalid field ('+ field +') in declaration');
-                } else {
-                    if (validationRules[field]) {
-                        var fieldErr = validationRules[field].valueValidation((decl[field]));
-
-                        (fieldErr || []).length && (errors.push(fieldErr));
-                    } else if (!~validDeclFields.indexOf(field)) {
-                        errors.push('Invalid field ('+ field +') in declaration');
-                    }
-                }
-            }, this);
-        } else {
-            var declType = toType(decl);
-
-            errors.push('Invalid declaration type ('+ declType +'), expected object');
-        }
-
-        if (errors.length) {
-            return errors;
-        }
-    }
-
-};
-
-
-/**
- * Export plugin for bemhint
- */
-var vm = require('vm');
+function concatExpected(old, add, delim) {
+    return old ?
+        old + (delim || ', ') + add :
+        add;
+}
 
 module.exports = {
     forEntityTech: function (tech, techsConfig, entity) {
-        if (tech.content) {
-            var deps = vm.runInThisContext(tech.content),
-                validateRes = validator.validate(deps);
-
-            if (validateRes) {
-                validateRes.forEach(function(error) {
-                    entity.addError({
-                        msg: 'Deps validation',
-                        tech: 'deps.js',
-                        value: error
-                    });
+        tech.content && topLevelDepsValidation(
+            vm.runInThisContext(tech.content), // TODO: catch errors
+            '',
+            function(error) {
+                entity.addError({
+                    msg : 'Deps validation',
+                    tech : 'deps.js',
+                    value : error
                 });
-            }
-        }
+            });
     }
 };
